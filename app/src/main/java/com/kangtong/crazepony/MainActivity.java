614d356d07b83f7808e92b349886c742bfd72c2b
TongKang <kangtong1993@outlook.com>
2019-05-05 00:24:23 +0800

    UI基本完善

--- app/src/main/java/com/kangtong/crazepony/MainActivity.java
+++ app/src/main/java/com/kangtong/crazepony/MainActivity.java
@@ -1,11 +1,8 @@
 package com.kangtong.crazepony;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
