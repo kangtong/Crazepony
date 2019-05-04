01bb096cc59337515026db5e4ffcb39b4717eaeb
TongKang <kangtong1993@outlook.com>
2019-05-04 20:53:15 +0800

    Initial commit

---
+++ app/src/main/java/com/kangtong/crazepony/MainActivity.java
@@ -0,0 +1,20 @@
+package com.kangtong.crazepony;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+    }
+
+}
